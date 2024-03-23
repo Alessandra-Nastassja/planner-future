e177639afbf06089b4fde6e0364315c400e51659
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2024-03-23 18:25:23 -0300

    feat: it was included salary session

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  faEye = faEye;
+  visualizarValores;
 
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    let localStorage = document.defaultView?.localStorage;
+    let setVisualizacaoValores: any;
+    
+    if (localStorage) {
+      setVisualizacaoValores = localStorage.getItem('visualizarValores');
+      this.visualizarValores = JSON.parse(setVisualizacaoValores);
+    }
+    
+  }
+
+  handleVisualizacaoValores(){
+    this.visualizarValores = !this.visualizarValores;
+    localStorage.setItem('visualizarValores', JSON.stringify(this.visualizarValores));
+  }
 }
