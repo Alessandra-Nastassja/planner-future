4bc6d9164a4987adeb164ba4a3524597116768ca
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2024-10-11 11:53:49 -0300

    feat: add account balance

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { MenuComponent } from '../../../shared/components/menu/menu.component';
 import { QuickSendsComponent } from './quick-sends/quick-sends.component';
 import { TotalBalanceComponent } from './total-balance/total-balance.component';
 import { RouterModule } from '@angular/router';
+import { AccountBalanceComponent } from "./account-balance/account-balance.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TotalBalanceComponent, QuickSendsComponent, MenuComponent, RouterModule],
+  imports: [TotalBalanceComponent, QuickSendsComponent, MenuComponent, RouterModule, AccountBalanceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
